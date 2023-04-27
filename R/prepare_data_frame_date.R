d3b135a181f38a56a49c916cd8a9e19fdc4e0df2
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2023-04-26 22:45:59 -0300

    perf: data.table to read files

--- R/prepare_data_frame_date.R
+++ R/prepare_data_frame_date.R
@@ -9,6 +9,7 @@
 #' @keywords internal
 prepare_data_frame_date <- function(nh3_merged, to_local = TRUE,
                                     col_names = c("DATE", "TIME", "NH3_30s")){
+  nh3_merged <- as.data.frame(nh3_merged)
   nh3_merged$date <- paste(nh3_merged$DATE, nh3_merged$TIME)
   nh3_merged$date <- as.POSIXct(strptime(nh3_merged$date,
                                          format = "%Y-%m-%d %H:%M:%OS"),
--- R/read_nh3_file.R
+++ R/read_nh3_file.R
@@ -6,6 +6,6 @@
 #' @noRd
 #' @keywords internal
 read_nh3_file <- function(file_path){
-  nh3_df <- utils::read.table(file_path, header = T, sep = "", dec = ".")
+  nh3_df <- data.table::fread(file_path, header = T, sep = " ", dec = ".")
   return(nh3_df)
 }
